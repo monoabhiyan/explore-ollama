bcb6c8505044d6c272582beb6eb97d29fa9de239
abhiyan <abhiyan.shrestha@monotype.com>
2026-01-03 13:34:03 +0545

    feat: add prints

--- src/index.py
+++ src/index.py
@@ -3,11 +3,14 @@ from db.vector_store import collection
 from pipeline.document_processor import process_and_add_documents
 from rag import rag_query
 
-# print("Indexing....")
-# process_and_add_documents(collection, DOCS_DIR)
+print("Indexing....")
+process_and_add_documents(collection, DOCS_DIR)
 
-query = "How many years of exp Abhiyan have and what are his skills, how many years Abhiyan have worked on  ?"
-response, sources = rag_query(collection, query)
+
+query = "1. abhiyan and nikesh work exp ?"
+
+print('Answering...')
+response, sources = rag_query(collection, query, n_chunks=5)
 
 # Print results
 print("\nQuery:", query)
